ea23eac6268acceae3e473e38378e1d9bccb4eeb
Miguel Borges <mrcb08022002@gmail.com>
2020-11-29 20:49:46 +0000

    Events even easier

--- main.m
+++ main.m
@@ -1,8 +1,10 @@
+world1 = [1 1 1 1 1 1 1 1; 1 0 0 0 0 0 0 1; 1 0 0 0 0 0 0 1; 1 1 1 1 1 1 0 1; 1 0 0 0 0 0 0 1; 1 0 0 1 0 0 0 5; 1 0 0 1 0 0 0 1; 1 1 1 1 1 1 1 1];
 pos = [2,2];
 world = 1;
+event = [0 0];
 while true
   clc
-  map = mkMap(pos,world);
+  map = mkMap(pos,world,world1);
   x = drawMap(map,pos);
   action = input('\n','s');
   [world pos map] = move(action,pos,map,world);
